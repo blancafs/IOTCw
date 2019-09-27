7c931a2b0a74a00a5a0121bc6eea741637c24c11
Vangelis <axo@beast.localdomain>
2019-09-27 18:16:18 +0100

    Added flask server

---
+++ iot_server/engine.py
@@ -0,0 +1,8 @@
+
+class Engine:
+
+    def __init__(self):
+        pass
+
+    def dealWithData(self, data):
+        return 'Yo'
---
+++ iot_server/server.py
@@ -0,0 +1,50 @@
+# Imports
+import os
+import pkg_resources
+from pkg_resources import DistributionNotFound, VersionConflict
+
+# dependencies can be any iterable with strings,
+# e.g. file line-by-line iterator
+dependencies = [
+  'Flask>=0.9'
+]
+
+# Check for dependencies
+try:
+# here, if a dependency is not met, a DistributionNotFound or VersionConflict
+# exception is thrown.
+    pkg_resources.require(dependencies)
+    print('Dependencies Met')
+except:
+    print('Flask was not found, installing it now ...')
+    os.system('pip3 install flask')
+
+# Custom imports
+from engine import Engine
+
+# Web imports
+from flask import Flask, render_template, flash, request, redirect, url_for, session
+
+# Initialize serving configuration
+app = Flask(__name__)
+data = 'data'
+PORT = 5000
+
+# Initialize engine
+engine = Engine()
+
+## ROUTES ##
+@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    # Receive string argument
+    arg = request.args.get(data)
+    print(arg)
+    info = engine.dealWithData(arg)
+    return str(info)
+
+
+### MAIN ###
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=PORT)
+    print('[main]: main(): Server is listening on port:', PORT)
